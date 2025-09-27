4ef0f1735dc70e7aebc36260c2ffeb7886d242d7
queenprotea <yizuzelbautista@gmail.com>
2025-09-26 18:39:48 -0600

    cracion de repositorio

---
+++ ClienteInventarioPlus/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+namespace ClienteInventarioPlus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ServidorInventarioPlus/App.xaml.cs
@@ -0,0 +1,9 @@
+namespace ServidorInventarioPlus
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
