f4d6d1d53d1a403b8874b4c48091f8f8581b1a43
isawi16 <isairamirez1607@gmail.com>
2025-09-27 23:35:08 -0600

    creacion de ventanas de iniciar sesion y menuprincipal (aun falta completar)

---
+++ ClienteInventarioPlus/Utilidades/Animaciones.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace ClienteInventarioPlus.Utilidades
+{
+    public class Animaciones
+    {
+        public static void IniciarAnimacion(UIElement elementoObjetivo, string claveRecurso, Action alCompletar = null)
+        {
+            if (elementoObjetivo == null || string.IsNullOrEmpty(claveRecurso)) return;
+
+            var animacion = Application.Current.Resources[claveRecurso] as Storyboard;
+            if (animacion != null)
+            {
+                var animacionClonada = animacion.Clone();
+
+                if (alCompletar != null)
+                    animacionClonada.Completed += (s, e) => alCompletar();
+
+                if (elementoObjetivo is FrameworkElement elementoMarco)
+                    animacionClonada.Begin(elementoMarco);
+            }
+        }
+        public static void SacudirTextBox(TextBox textBox)
+        {
+            if (textBox == null) return;
+
+            var gridPrincipal = (Grid)textBox.Template.FindName("MainGrid", textBox);
+
+            if (gridPrincipal != null)
+            {
+                var guionAnimacion = Application.Current.Resources["ShakeAnimation"] as Storyboard;
+                if (guionAnimacion != null)
+                {
+                    guionAnimacion.Begin(gridPrincipal, true);
+                    return;
+                }
+            }
+
+            if (textBox.RenderTransform == null || !(textBox.RenderTransform is TranslateTransform))
+                textBox.RenderTransform = new TranslateTransform();
+
+            IniciarAnimacion(textBox, "ShakeAnimation");
+        }
+        public static void SacudirPasswordBox(PasswordBox passwordBox)
+        {
+            if (passwordBox == null) return;
+
+            if (passwordBox.RenderTransform == null || !(passwordBox.RenderTransform is TranslateTransform))
+                passwordBox.RenderTransform = new TranslateTransform();
+
+            IniciarAnimacion(passwordBox, "ShakeAnimation");
+        }
+
+        public static void SacudirDatePicker(DatePicker datePicker)
+        {
+            if (datePicker == null) return;
+
+            if (datePicker.RenderTransform == null || !(datePicker.RenderTransform is TranslateTransform))
+                datePicker.RenderTransform = new TranslateTransform();
+
+            IniciarAnimacion(datePicker, "ShakeAnimation");
+        }
+    }
+}
---
+++ ClienteInventarioPlus/Vistas/MenuPrincipalAdministrador.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using BibliotecaClasesNetframework.ModelosODT;
+using MessageBox = Xceed.Wpf.Toolkit.MessageBox;
+
+namespace ClienteInventarioPlus.Vistas
+{
+    public partial class MenuPrincipalAdministrador : UserControl
+    {
+        private MainWindow _mainWindow;
+        //private IAhorcadoService proxy;
+        private UsuarioDTO usuarioSesion;
+
+        /*public MenuPrincipalAdministrador(MainWindow mainWindow, UsuarioDTO usuario, IAhorcadoService proxy)
+        {
+            try
+            {
+                InitializeComponent();
+                _mainWindow = mainWindow;
+                
+                usuarioSesion = usuario;
+                this.proxy = proxy;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al conectar con el servicio: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }*/
+        
+        public MenuPrincipalAdministrador(MainWindow mainWindow)
+        {
+            try
+            {
+                InitializeComponent();
+                _mainWindow = mainWindow;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al conectar con el servicio: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        //menu lateral
+        private void BtnProductos_Click(object sender, RoutedEventArgs e)
+        {
+            //_mainWindow.CambiarVista(new ProductoPrincipal(_mainWindow, jugadorSesion, proxy));
+        }
+
+        private void BtnMovimientos_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnReservas_Click(object sender, RoutedEventArgs e)
+        {
+            //_mainWindow.CambiarVista(new ReservaPrincipal(_mainWindow, jugadorSesion, proxy));
+        }
+
+        private void BtnReportes_Click(object sender, RoutedEventArgs e)
+        {
+            //_mainWindow.CambiarVista(new ConsultarHistorialPartidasUserControl(_mainWindow, jugadorSesion, proxy));
+        }
+
+        private void BtnProveedores_Click(object sender, RoutedEventArgs e)
+        {
+            //_mainWindow.CambiarVista(new MarcadoresUserControl(_mainWindow, jugadorSesion, proxy));
+        }
+        
+        private void BtnUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+            //_mainWindow.CambiarVista(new MarcadoresUserControl(_mainWindow, jugadorSesion, proxy));
+        }
+
+        /*private void BtnCerrarSesion_Click(object sender, RoutedEventArgs e)
+        {
+            _mainWindow.CambiarVista(new IniciarSesion(_mainWindow, proxy));
+        }*/
+        
+        private void BtnCerrarSesion_Click(object sender, RoutedEventArgs e)
+        {
+            _mainWindow.CambiarVista(new IniciarSesion(_mainWindow));
+        }
+        
+    }
+}
